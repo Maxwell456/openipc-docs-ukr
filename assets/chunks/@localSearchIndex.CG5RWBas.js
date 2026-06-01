ec39846860abc08f91d0bd7cad69e84dc23fa53e
Maxwell456 <Maxwell456@users.noreply.github.com>
2026-06-01 16:42:40 +0000

    deploy: f04515e572e64c9957b19b7f2bcf554f860b0702

--- assets/chunks/@localSearchIndex.CG5RWBas.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./framework.rlpw0eGz.js";const r={root:()=>_(()=>import("./@localSearchIndexroot.D7j7tqpW.js"),[]),en:()=>_(()=>import("./@localSearchIndexen.8GVzkE8V.js"),[])};export{r as default};
